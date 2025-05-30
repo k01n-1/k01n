a4b74a98f371f150de01617892139161000b42c4
k01n-1 <iv4nkostyuk@yandex.ru>
2025-05-31 00:08:06 +0800

    Add files via upload

--- Web_2_FinalL/script.js
+++ Web_2_FinalL/script.js
@@ -35,3 +35,4 @@ document.getElementById('calculator-form').addEventListener('submit', function(e
     // Сохраняем данные (опционально)
     localStorage.setItem('lastCalculation', JSON.stringify({ siteType, pages, design, seo, price }));
 });
+
